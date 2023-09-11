21fc0fde58b36257385d2883eac46dfc5e8a6f75
stans184 <life4603@naver.com>
2023-09-11 18:57:55 +0900

    ..

--- ALL_THE_TOP/src/main/java/com/app/alltt/community/service/CommunityService.java
+++ ALL_THE_TOP/src/main/java/com/app/alltt/community/service/CommunityService.java
@@ -6,7 +6,6 @@ import com.app.alltt.community.dto.PostDTO;
 import com.app.alltt.community.dto.RecmndDTO;
 import com.app.alltt.community.dto.ReplyDTO;
 import com.app.alltt.crawling.dto.ContentDTO;
-import com.app.alltt.main.dto.FilteredDTO;
 import com.app.alltt.member.dto.MemberDTO;
 
 public interface CommunityService {
@@ -55,6 +54,7 @@ public interface CommunityService {
 	// 댓글 전체 삭제
 	public boolean removeAllReply(long memberId);
 
-	public boolean insertWishContent(FilteredDTO wishDTO);
+	public boolean checkWishContent(long memberId, long contentId);
+	public boolean modifyWishContent(long memberId, long contentId);
 
 }
